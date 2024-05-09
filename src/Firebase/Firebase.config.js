033699a6958d335df79773de08b6bbae8072cdb5
Aksamrat <samratuap52@gmail.com>
2024-05-09 22:10:25 +0600

    first commit

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyD-zSK45Weqg6W3d3X_KIcWKRd6jHlVazw',
+  authDomain: 'expert-hunter.firebaseapp.com',
+  projectId: 'expert-hunter',
+  storageBucket: 'expert-hunter.appspot.com',
+  messagingSenderId: '140822698995',
+  appId: '1:140822698995:web:6a6dd566b61b051c7f1086',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/main.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import Roots from './Component/Roots.jsx';
+import ErrorPage from './Pages/ErrorPage.jsx';
+import HomePage from './Pages/HomePage.jsx';
+import LoginPage from './Pages/LoginPage.jsx';
+import Register from './Pages/Register.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Roots />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        path: '/',
+        element: <HomePage />,
+      },
+      {
+        path: '/login',
+        element: <LoginPage />,
+      },
+      {
+        path: '/register',
+        element: <Register />,
+      },
+      // {
+      //   path: '/job/:id',
+      //   element: <JobDetails />,
+      //   loader: ({ params }) =>
+      //     fetch(`${import.meta.env.VITE_API_URL}/job/${params.id}`),
+      // },
+      // {
+      //   path: '/allJobs',
+      //   element: <ALlJobs />,
+      // },
+      // {
+      //   path: '/addJob',
+      //   element: <AddJob />,
+      // },
+      // {
+      //   path: '/myPostedJob',
+
+      //   element: (
+      //     <PrivateRoute>
+      //       <MyPostedJob />
+      //     </PrivateRoute>
+      //   ),
+      // },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <AuthProvider> */}
+    {/* <QueryClientProvider client={queryClient}> */}
+    <RouterProvider router={router} />
+    {/* </QueryClientProvider> */}
+    <ToastContainer />
+    {/* </AuthProvider> */}
+  </React.StrictMode>
+);
